567ff9d882de84ce073f556cafa7b59eeac3b70b
paul <paul@pauls-MacBook-Pro.local>
2017-07-30 13:24:53 -0400

    fixed paths

--- src/components/conceptCloud.js
+++ src/components/conceptCloud.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import SliderDeck, {Button} from './controls/SliderDeck'
-import Slider from './controls/Slider'
-import forceSetup from './force_setup/index'
-import * as forces from './force_setup/forces/forces';
-import ContentAddBox from 'material-ui/svg-icons/content/add-box';
-import * as c from 'material-ui/styles/colors';
+import React from "react";
+import SliderDeck, { Button } from "./controls/SlideControls";
+import Slider from "./controls/ForceSlider";
+import forceSetup from "./force_setup/index";
+import * as forces from "./force_setup/forces/forces";
+import ContentAddBox from "material-ui/svg-icons/content/add-box";
+import * as c from "material-ui/styles/colors";
 
 export class ForceLayout extends React.Component {
   constructor(props) {
     super(props);
     this.simulation = {};
     this.state = {
-      startCount: 100,
-      startSize: parseInt(window.localStorage.getItem('startSize')) || 30,
+      startCount: 50,
+      startSize: parseInt(window.localStorage.getItem("startSize")) || 30,
       center: {
         on: true,
         strength: 1
@@ -21,7 +21,7 @@ export class ForceLayout extends React.Component {
         on: true,
         strength: 10
       }
-    }
+    };
     forceSetup(this);
     window.d = this;
   }
@@ -36,31 +36,47 @@ export class ForceLayout extends React.Component {
   //   this.simulation = this.doForce(this.state.startCount, this.state.startSize);
   // }
 
-  updateForce = (force) => (newState) => {
+  updateForce = force => newState => {
     let obj = {};
     obj[force] = Object.assign({}, this.state[force], newState);
 
     this.setState(obj);
-    this
-      .simulation
-      .force(force, forces[force](this.state[force].strength))
-  }
+    this.simulation.force(force, forces[force](this.state[force].strength));
+  };
 
   updateSize = (e, startSize) => {
-    this.setState({startSize});
-    window
-      .localStorage
-      .setItem('startSize', startSize);
-  }
+    this.setState({ startSize });
+    window.localStorage.setItem("startSize", startSize);
+  };
 
   render() {
-    let {center, startSize, charge} = this.state
+    let { center, startSize, charge } = this.state;
     return (
       <div id="holder">
         <SliderDeck>
-          <Slider onSlide={this.updateSize} value={startSize} min={10} max={200} label="size"/>
-          <Slider value={center.strength} min={-50} max={50} defaultValue={1} update={this.updateForce('center')} label="center"/>
-          <Slider min={-50} max={50} value={charge.strength} defaultValue={0} update={this.updateForce('charge')} label="charge"/>
+          <Slider
+            onSlide={this.updateSize}
+            value={startSize}
+            min={10}
+            max={200}
+            label="size"
+          />
+          <Slider
+            value={center.strength}
+            min={-50}
+            max={50}
+            defaultValue={1}
+            update={this.updateForce("center")}
+            label="center"
+          />
+          <Slider
+            min={-50}
+            max={50}
+            value={charge.strength}
+            defaultValue={0}
+            update={this.updateForce("charge")}
+            label="charge"
+          />
         </SliderDeck>
       </div>
     );
--- src/components/controls/SlideControls.js
+++ src/components/controls/SlideControls.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import Slider from './Slider'
-import FlatButton from 'material-ui/FlatButton';
+import React from "react";
+import Slider from "./ForceSlider";
+import FlatButton from "material-ui/FlatButton";
 
 export function Button(props) {
-  return (<FlatButton label={props.label}/>)
+  return <FlatButton label={props.label} />;
 }
 
 export default function SliderDeck(props) {
-
   return (
     <div className="slider-deck">
       {props.children}
     </div>
-
   );
 }
